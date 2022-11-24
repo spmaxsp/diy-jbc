3b322b077e42630d636beaaf5f875a009247b635
spmaxsp <mexx@spahn-rodgau.de>
2022-11-24 12:37:38 +0100

    Implementing read_loop for serial interface

--- ser_interface.cpp
+++ ser_interface.cpp
@@ -5,3 +5,25 @@ SerialInterface::SerialInterface(Stream* serStreamRef, State* state){
   this->state = state;
   this->buf_cursor = 0;
 }
+
+void SerialInterface::phrase_command(){
+    //Commands:
+    //  -SET TEMP 100
+    //  -ENABLE
+    //  -DISABLE    
+}
+
+void SerialInterface::read_loop(){
+  char in_char;
+  while (this->serStreamRef->available() > 0) {
+    in_char = this->serStreamRef->read();
+    if (in_char == '\n') {
+      this->phrase_command();
+      this->buf_cursor = 0;
+    } else {        
+      if (this->buf_cursor < MAX_MESSAGE) {
+        this->buffer[this->buf_cursor++] = in_char;
+      }
+    }
+  }
+}
--- ser_interface.h
+++ ser_interface.h
@@ -3,15 +3,20 @@
 #include <Arduino.h>
 #include "state.h"
 
+#define MAX_MESSAGE 30
+
 class SerialInterface { 
   private: 
     char buffer[10];
     int buf_cursor;
     Stream *serStreamRef;
     State *state;
+    
+    void phrase_command();
 
   public:  
-      SerialInterface(Stream* serStreamRef, State* state);
+    SerialInterface(Stream* serStreamRef, State* state);
+    void read_loop();
 };
 
 #endif
