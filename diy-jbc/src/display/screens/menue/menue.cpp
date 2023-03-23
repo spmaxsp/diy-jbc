546e49cb035f819d347f2ad5fbe6719e03fb7ad7
spmaxsp <mexx@spahn-rodgau.de>
2023-03-23 10:00:58 +0100

    completely changing the menue structure

--- diy-jbc/src/display/screens/menue/menue.cpp
+++ diy-jbc/src/display/screens/menue/menue.cpp
@@ -3,114 +3,54 @@
 MenueScreen::MenueScreen(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref){
   this->u8g2ref = u8g2ref;
 
-  submenue functions_menue = {
-    "Functions",
-    0,
-    false,
-    {
-      {"Sleep D. (s)", 0, "i", 1, 3600},
-      {"Sleep T. (C)", 0, "f", 0, 500},
-      {"Hib. D. (s)", 0, "i", 1, 3600},
-      {"Hib T. (C)", 0, "f", 0, 500}
-    }
-  };
-  submenue pid_menue = {
-    "PID",
-    0,
-    false,
-    {
-      {"KP", 0, "f", 0, 100},
-      {"KI", 0, "f", 0, 100},
-      {"KD", 0, "f", 0, 100}
-    }
-  };
-  submenue temp_calib_menue = {
-    "Temp. Calibration",
-    0,
-    false,
-    {
-      {"A", 0, "f", -100, 100},
-      {"B", 0, "f", -100, 100},
-      {"C", 0, "f", -100, 100},
-    }
-  };
-  mainmenue menue_structure = {
-    "Main Menue",
-    0,
-    false,
-    {functions_menue,pid_menue,temp_calib_menue}
-  };
+  this->menue_structure = new SubMenue("Main Menue", 3);
+
+  SubMenue* functions_menue = new SubMenue("Functions", 4);
+  functions_menue->add_menueelement(new MenueItem("Sleep D. (s)", 0, 'i', 3600.0, 1));
+  functions_menue->add_menueelement(new MenueItem("Sleep T. (C)", 0, 'f', 500, 0));
+  functions_menue->add_menueelement(new MenueItem("Hib. D. (s)", 0, 'i', 3600, 1));
+  functions_menue->add_menueelement(new MenueItem("Hib T. (C)", 0, 'f', 500, 0));
+
+  SubMenue* pid_menue = new SubMenue("PID", 3); 
+  pid_menue->add_menueelement(new MenueItem("KP", 0, 'f', 100, 0));
+  pid_menue->add_menueelement(new MenueItem("KI", 0, 'f', 100, 0));
+  pid_menue->add_menueelement(new MenueItem("KD", 0, 'f', 100, 0));
+
+  SubMenue* temp_calib_menue = new SubMenue("Temp. Calibration", 3);
+  temp_calib_menue->add_menueelement(new MenueItem("A", 0, 'f', 100, -100));
+  temp_calib_menue->add_menueelement(new MenueItem("B", 0, 'f', 100, -100));
+  temp_calib_menue->add_menueelement(new MenueItem("C", 0, 'f', 100, -100));
+
+  this->menue_structure->add_menueelement(functions_menue);
+  this->menue_structure->add_menueelement(pid_menue);
+  this->menue_structure->add_menueelement(temp_calib_menue);
 }
 
 void MenueScreen::display(){
-  if (menue_structure.active){
-    display_menue(&menue_structure);
-  }
-  else{
-    submenue* submenue = &menue_structure.menue_items[menue_structure.selected];
-    if (submenue->active){
-      display_submenue(submenue);
-    }
-    else{
-      menueitem* menueitem = &submenue->menue_items[submenue->selected];
-      display_menueitem(menueitem);
-    }
-  }
+
 }
 
-void MenueScreen::setup_encoder(Input* input){
-  if (menue_structure.active){
-    input->change_rotary_value(menue_structure.selected);
-    input->change_rotary_min_max(0,sizeof(menue_structure.menue_items)-1);
-  }
-  else{
-    submenue* submenue = &menue_structure.menue_items[menue_structure.selected];
-    if (submenue->active){
-      input->change_rotary_value(submenue->selected);
-      input->change_rotary_min_max(0,sizeof(submenue->menue_items)-1);
-    }
-    else{
-      menueitem* menueitem = &submenue->menue_items[submenue->selected];
-      input->change_rotary_value(menueitem->value);
-      input->change_rotary_min_max(menueitem->min_value,menueitem->max_value);
-    }
-  }
+SubMenue::SubMenue(char* title, int size){
+  strcpy(this->title, title);
+  this->enty_selected = 0;
+  this->selected_entry_active = false;
+  this->scroll_offset = 0;
+  this->menueelement_count = 0;
+  this->menueelement_max = size;
+  this->menueelements = new MenueElement*[size];
 }
 
-bool MenueScreen::handle_input(Input* input){
-  if (menue_structure.active){
-    if(input->button_red_pressed){
-      return false;
-    }
-    else if (input->button_blue_pressed or input->button_rotary_pressed){
-      menue_structure.active = false;
-      menue_structure.menue_items[menue_structure.selected].active = true;
-      this->setup_encoder(input);
-    } 
-    menue_structure.selected = input->encoder_pos;
+void SubMenue::add_menueelement(MenueElement* menueelement){
+  if (this->menueelement_count >= this->menueelement_max){
+    return;
   }
-  else{
-    submenue* submenue = &menue_structure.menue_items[menue_structure.selected];
-    if (submenue->active){
-      if(input->button_red_pressed){
-        submenue->active = false;
-        menue_structure.active = true;
-        this->setup_encoder(input);
-      }
-      else if (input->button_blue_pressed or input->button_rotary_pressed){
-        submenue->active = false;
-        this->setup_encoder(input);
-      } 
-      submenue->selected = input->encoder_pos;
-    }
-    else{
-    
-    }
-  }
-  return true;
+  this->menueelements[this->menueelement_count] = menueelement;
+  this->menueelement_count++;
 }
 
-void MenueScreen::display_menue(mainmenue* menue){
+
+
+void SubMenue::diplay_title(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref){
   u8g2ref->setFontMode(1);
 
   u8g2ref->setDrawColor(1);
@@ -121,57 +61,62 @@ void MenueScreen::display_menue(mainmenue* menue){
 
   u8g2ref->setDrawColor(0);
   u8g2ref->setFont(u8g2_font_ncenB08_tr);
-  u8g2ref->drawStr( 0, 12, "Main Menue");
+  u8g2ref->drawStr( 0, 12, this->title);
+}
 
+void SubMenue::display_scrollbar(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref){
   u8g2ref->setDrawColor(1);
   u8g2ref->drawBox(u8g2ref->getDisplayWidth()-15, 25, 6, 26);
   u8g2ref->setFont(u8g2_font_unifont_t_symbols);
   u8g2ref->drawGlyph(u8g2ref->getDisplayWidth()-16, 25, 9652);
   u8g2ref->drawGlyph(u8g2ref->getDisplayWidth()-16, 60, 9662);
-  
+}
+
+void SubMenue::display_entries(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref){
+}
+
+void SubMenue::display_as_entry(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref, int y){
   u8g2ref->setDrawColor(1);
   u8g2ref->setFont(u8g2_font_profont12_mr);
-  u8g2ref->drawStr( 10, 30, "Entry 1");
-  u8g2ref->drawStr( 10, 40, "Entry 2");
-  u8g2ref->drawStr( 10, 50, "Entry 3");
-  u8g2ref->drawStr( 10, 60, "Entry 4");
-  
-  u8g2ref->setDrawColor(1);
-  u8g2ref->setFont(u8g2_font_unifont_t_symbols);
-  u8g2ref->drawGlyph(0, 40+2, 9658);
+  u8g2ref->drawStr( 10, y, this->title);
 }
 
-void MenueScreen::display_submenue(submenue* menue){
-  u8g2ref->setFontMode(1);
+void SubMenue::display(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref){
 
-  u8g2ref->setDrawColor(1);
-  u8g2ref->drawBox(0, 0, u8g2ref->getDisplayWidth(), 13);
+}
 
-  u8g2ref->setDrawColor(1);
-  u8g2ref->drawHLine(0, 15, u8g2ref->getDisplayWidth());
-
-  u8g2ref->setDrawColor(0);
-  u8g2ref->setFont(u8g2_font_ncenB08_tr);
-  u8g2ref->drawStr( 0, 12, "Main Menue");
-
-  u8g2ref->setDrawColor(1);
-  u8g2ref->drawBox(u8g2ref->getDisplayWidth()-15, 25, 6, 26);
-  u8g2ref->setFont(u8g2_font_unifont_t_symbols);
-  u8g2ref->drawGlyph(u8g2ref->getDisplayWidth()-16, 25, 9652);
-  u8g2ref->drawGlyph(u8g2ref->getDisplayWidth()-16, 60, 9662);
+bool SubMenue::handle_input(Input* input){
   
+}
+
+
+MenueItem::MenueItem(char* title, double value, char type, double max_value, double min_value){
+  strcpy(this->title, title);
+  this->value = value;
+  this->type = type;
+  this->max_value = max_value;
+  this->min_value = min_value;
+}
+
+void MenueItem::display_as_entry(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref, int y){
   u8g2ref->setDrawColor(1);
   u8g2ref->setFont(u8g2_font_profont12_mr);
-  u8g2ref->drawStr( 10, 30, "Entry 1");
-  u8g2ref->drawStr( 10, 40, "Entry 2");
-  u8g2ref->drawStr( 10, 50, "Entry 3");
-  u8g2ref->drawStr( 10, 60, "Entry 4");
-  
+  u8g2ref->drawStr( 10, y, this->title);
+}
+
+void MenueItem::display(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref){
+  char buf[5];
+  snprintf (buf, 5, "%d", this->value);
   u8g2ref->setDrawColor(1);
-  u8g2ref->setFont(u8g2_font_unifont_t_symbols);
-  u8g2ref->drawGlyph(0, 40+2, 9658);
+  u8g2ref->setFont(u8g2_font_profont12_mr);
+  u8g2ref->drawStr( 10, 25, this->title);
+  u8g2ref->drawStr( 10, 40, buf);
 }
 
-void MenueScreen::display_menueitem(menueitem* item){
+bool MenueItem::handle_input(Input* input){
 
 }
+
+
+
+
--- diy-jbc/src/display/screens/menue/menue.h
+++ diy-jbc/src/display/screens/menue/menue.h
@@ -5,36 +5,56 @@
 
 #include "../../screenmanager.h"
 
-typedef struct {
-  char title[20];
-  double value;
-  char type;
-  double max_value;
-  double min_value;
-} menueitem;
+class MenueElement {
+  public:
+    void virtual display(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref)=0;
+    bool virtual handle_input(Input* input)=0;
+    void virtual display_as_entry(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref, int y)=0;
+    void virtual add_menueelement(MenueElement* menueelement);
+};
 
-typedef struct {
-  char title[20];
-  int selected;
-  bool active;
-  menueitem menue_items[];
-} submenue;
+void MenueElement::add_menueelement(MenueElement* menueelement){
+  return;
+}
 
-typedef struct {
-  char title[20];
-  int selected;
-  bool active;
-  submenue menue_items[];
-} mainmenue;
+class MenueItem: public MenueElement {
+  private:
+    char title[20];
+    double value;
+    char type;
+    double max_value;
+    double min_value;
+  public:
+    MenueItem(char* title, double value, char type, double max_value, double min_value);
+    void display_as_entry(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref, int y);
+    void display(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref);
+    bool handle_input(Input* input);
+};
+
+class SubMenue: public MenueElement {
+  private:
+    char title[20];
+    int enty_selected;
+    bool selected_entry_active;
+    int scroll_offset;
+    int menueelement_count;
+    int menueelement_max;
+    MenueElement** menueelements;
+    void diplay_title(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref);
+    void display_scrollbar(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref);
+    void display_entries(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref);
+  public:
+    SubMenue(char* title, int size);
+    void add_menueelement(MenueElement* menueelement);
+    void display_as_entry(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref, int y);
+    void display(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref);
+    bool handle_input(Input* input);
+};
 
 class MenueScreen: public Screen {
   private:
     U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref;
-    void display_menue(mainmenue* menue);
-    void display_submenue(submenue* menue);
-    void display_menueitem(menueitem* item);
-    void setup_encoder(Input* input);
-    mainmenue menue_structure;
+    MenueElement* menue_structure;
   public:
     MenueScreen(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref);
     void display();
