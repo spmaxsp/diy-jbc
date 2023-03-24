d43a523f3c1b3f421bba147c97491b5bb4cc1b70
spmaxsp <mexx@spahn-rodgau.de>
2023-03-24 16:59:43 +0100

    adding basic main_screen

--- diy-jbc/src/display/screens/mainscreen/mainscreen.cpp
+++ diy-jbc/src/display/screens/mainscreen/mainscreen.cpp
@@ -3,3 +3,17 @@
 MainScreen::MainScreen(U8G2_SH1106_128X64_NONAME_1_HW_I2C* u8g2ref){
   this->u8g2ref = u8g2ref;
 }
+
+void MainScreen::display(){
+  u8g2ref->clearBuffer();
+  u8g2ref->setFont(u8g2_font_6x10_tf);
+  u8g2ref->drawStr(0, 10, "Main Screen");
+  u8g2ref->sendBuffer();
+}
+
+bool MainScreen::handle_input(Input* input){
+  if (input->button_blue_pressed or input->button_rotary_pressed){
+    return false;
+  }
+  return true;  
+}
