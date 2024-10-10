48c39bf71a45897016057e52d33bec97cf8b62fe
spmaxsp <mexx@spahn-rodgau.de>
2024-10-10 12:55:37 +0200

    fixing encoder and adding relative movement

--- Core/Src/encoder.c
+++ Core/Src/encoder.c
@@ -1,18 +1,22 @@
 #include "encoder.h"
 
+#include <math.h>
+
 // Private variables
-static int16_t min = 0;
-static int16_t max = 0;
+int16_t min = 0;
+int16_t max = 0;
+
+const float acel = 1.4;
 
 // Public functions
 void Encoder_SetMinMax(int16_t new_min, int16_t new_max)
 {
     if (new_min < new_max)
     {
-        min = min;
-        max = max;
+        min = new_min;
+        max = new_max;
 
-        LL_TIM_SetAutoReload(TIM_ENCODER, max - min);
+        LL_TIM_SetAutoReload(TIM_ENCODER, (max - min)*2);
     }
 }
 
@@ -20,19 +24,34 @@ void Encoder_SetCounter(int16_t value)
 {
     if (value >= min && value <= max)
     {
-        LL_TIM_SetCounter(TIM_ENCODER, value);
+        LL_TIM_SetCounter(TIM_ENCODER, value*2);
     }
     else if (value < min)
     {
-        LL_TIM_SetCounter(TIM_ENCODER, min);
+        LL_TIM_SetCounter(TIM_ENCODER, min*2);
     }
     else
     {
-        LL_TIM_SetCounter(TIM_ENCODER, max);
+        LL_TIM_SetCounter(TIM_ENCODER, max*2);
     }
 }
 
 int16_t Encoder_GetCounter(void)
 {
-    return LL_TIM_GetCounter(TIM_ENCODER);
+    return min + (LL_TIM_GetCounter(TIM_ENCODER)/2);
+}
+
+
+int16_t Encoder_ResetAndGetDelta(void)
+{
+    int16_t delta = Encoder_GetCounter();
+    Encoder_SetCounter(0);
+    return delta;
+}
+
+int16_t Encoder_ResetAndGetDelta_Acel(void)
+{
+    int16_t delta = Encoder_GetCounter();
+    Encoder_SetCounter(0);
+    return floor(delta*delta*acel);
 }
